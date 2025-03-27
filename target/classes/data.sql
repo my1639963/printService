f77420c05ebd1b9b094a1848755987a5b13c9c47
my11639963 <5926172@qq.com>
2025-03-28 01:33:18 +0800

    删除错误提交的文件

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
--- 插入测试打印机数据
-INSERT INTO printer (name, model, ip_address, port, status, last_online_time) VALUES
-('测试打印机1', 'Pantum P2500', '192.168.1.100', 9100, 1, CURRENT_TIMESTAMP),
-('测试打印机2', 'Pantum P3300', '192.168.1.101', 9100, 1, CURRENT_TIMESTAMP),
-('测试打印机3', 'Pantum P3500', '192.168.1.102', 9100, 0, NULL); 
